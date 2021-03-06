615b13ec573880e078e7f531f4da129b88295aa9
elementrix05 <65112923+elementrix05@users.noreply.github.com>
2021-03-06 18:27:21 +0530

    Add files via upload

---
+++ routes/show.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const File = require('../models/file');
+
+router.get('/:uuid', async (req, res) => {
+    try {
+        const file = await File.findOne({ uuid: req.params.uuid });
+        if(!file) {
+            return res.render('download', { error: 'Link has been Expired !'});
+
+        }
+
+        return res.render('download', {
+            uuid: file.uuid,
+            filename: file.filename,
+            fileSize: file.size,
+            downolad: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
+        });
+    } catch(err) {
+        return res.render('download', { error: 'something went wrong :/'});
+    }
+    
+
+});
+
+
+module.exports = router ;
